60dd56fa3c52207e0b9e8041496f59fe485f0c3a
Luis Eduardo Gritz <luis.gritz@primecontrol.com.br>
2020-05-04 20:05:13 -0300

    Scripts

---
+++ Scripts/SCRIPT_PERGUNTAS.sql
@@ -0,0 +1,46 @@
+/* PERGUNTAS */
+
+--01 - QUAL A POSIÇÃO NO BRASIL EM 30/04/2020?
+SELECT D.name [PAÍS], E.dateUpdate [ATUALIZAÇÃO], H.amount [TOTAL], I.amount [MORTES], J.amount [RECUPERADOS], K.amount [ATIVOS], L.name [ARQUIVO], M.recordedDt [PROCESSAMENTO] FROM Daily AS A
+	INNER JOIN CountryRegion AS D
+		ON D.id = A.CountryRegionId
+	INNER JOIN LastUpdate AS E
+		ON E.id = A.LastUpdateId
+	INNER JOIN Confirmed AS H
+		ON H.id = A.ConfirmedId
+	INNER JOIN Deaths AS I
+		ON I.id = A.DeathsId
+	INNER JOIN Recovered AS J
+		ON J.id = A.RecoveredId
+	INNER JOIN Active AS K
+		ON K.id = A.ActiveId
+	INNER JOIN Archive AS L
+		ON L.id = A.ArchiveId
+	INNER JOIN Recorded AS M
+		ON M.id = A.RecordedId
+WHERE E.dateUpdate BETWEEN '30-04-2020' AND '01-05/2020'
+	AND D.name = 'BRAZIL'
+
+
+--02 - QUAL O NÚMERO DE MORTES ENTRE 15/04 E 30/04 NO BRASIL?
+SELECT D.name [PAÍS], /*E.dateUpdate [ATUALIZAÇÃO], H.amount [TOTAL],*/ (MAX(I.amount) - MIN(I.amount)) [MORTES]/*, J.amount [RECUPERADOS], K.amount [ATIVOS], L.name [ARQUIVO], M.recordedDt [PROCESSAMENTO]*/ FROM Daily AS A
+	INNER JOIN CountryRegion AS D
+		ON D.id = A.CountryRegionId
+	INNER JOIN LastUpdate AS E
+		ON E.id = A.LastUpdateId
+	INNER JOIN Confirmed AS H
+		ON H.id = A.ConfirmedId
+	INNER JOIN Deaths AS I
+		ON I.id = A.DeathsId
+	INNER JOIN Recovered AS J
+		ON J.id = A.RecoveredId
+	INNER JOIN Active AS K
+		ON K.id = A.ActiveId
+	INNER JOIN Archive AS L
+		ON L.id = A.ArchiveId
+	INNER JOIN Recorded AS M
+		ON M.id = A.RecordedId
+WHERE D.name = 'BRAZIL'
+	AND E.dateUpdate BETWEEN '15-04-2020' AND '01-05/2020'
+GROUP BY D.name
+
